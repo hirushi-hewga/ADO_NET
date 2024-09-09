76af26fab8754350cf8362c096b4069c4e843013
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-09-09 08:53:47 -0700

    Add project files.

---
+++ 2024-09-09 - Lesson (Connection Mode)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2024_09_09___Lesson__Connection_Mode_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
