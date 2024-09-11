bbff6186e6a7c43716755e62a01d5d5681460fb0
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-09-11 10:07:56 -0700

    HW (ConnectionMode)

---
+++ 2024-09-09 - HW (Connection Mode)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2024_09_09___HW__Connection_Mode_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
