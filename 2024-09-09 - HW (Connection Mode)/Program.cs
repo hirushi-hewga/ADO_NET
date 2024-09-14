e0d896564262cd8ac5ca6ac250a55b51a408e74f
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-09-14 15:34:00 -0700

    HW (ConnectionMode)

--- 2024-09-09 - HW (Connection Mode)/Program.cs
+++ 2024-09-09 - HW (Connection Mode)/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace _2024_09_09___HW__Connection_Mode_
+﻿using System.Data.SqlClient;
+
+namespace _2024_09_09___HW__Connection_Mode_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string connectionString = "workstation id=WorkersDatabase2024-08-19.mssql.somee.com;" +
+                "packet size=4096;" +
+                "user id=Hirushi_SQLLogin_2;" +
+                "pwd=wvrcqrk394;" +
+                "data source=WorkersDatabase2024-08-19.mssql.somee.com;" +
+                "persist security info=False;" +
+                "initial catalog=WorkersDatabase2024-08-19;" +
+                "TrustServerCertificate=True";
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+
+
+
+
+            Console.WriteLine("HPPS");
+
+
+
+
+            sqlConnection.Close();
         }
     }
 }
