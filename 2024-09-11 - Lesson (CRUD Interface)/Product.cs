70377546f24b12bbd3f1442e2dd79fc807360cda
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-09-25 09:41:12 -0700

    Lesson (CRUDInterface)

---
+++ 2024-09-11 - Lesson (CRUD Interface)/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024_09_11___Lesson__CRUD_Interface_
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Quantity { get; set; }
+        public int CostPrice { get; set; }
+        public string Producer { get; set; }
+        public int Price { get; set; }
+    }
+}
--- 2024-09-11 - Lesson (CRUD Interface)/Program.cs
+++ 2024-09-11 - Lesson (CRUD Interface)/Program.cs
@@ -34,9 +34,31 @@ namespace _2024_09_11___Lesson__CRUD_Interface_
             int rows = command.ExecuteNonQuery();
             Console.WriteLine(rows + " rows affected!");
         }
-        public void GetALL()
+        public List<Product> GetALL()
         {
+            string cmdText = @"select * from Products";
 
+            SqlCommand command = new SqlCommand(cmdText, conn);
+
+            SqlDataReader reader = command.ExecuteReader();
+            List<Product> products = new List<Product>();
+
+            while (reader.Read())
+            {
+                products.Add(new Product()
+                {
+                    Id = (int)reader[0],
+                    Name = (string)reader[1],
+                    Type = (string)reader[2],
+                    Quantity = (int)reader[3],
+                    CostPrice = (int)reader[4],
+                    Producer = (string)reader[5],
+                    Price = (int)reader[6],
+                });
+            }
+
+            reader.Close();
+            return products;
         }
         public void Update()
         {
@@ -52,13 +74,18 @@ namespace _2024_09_11___Lesson__CRUD_Interface_
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;
-                                        Initial Catalog=master;
+            string connectionString = @"Data Source=WINDEV2401EVAL\SQLEXPRESS;
+                                        Initial Catalog=SportShop;
                                         Integrated Security=True;
                                         Connect Timeout=30;
                                         Encrypt=False;";
             SportShop sportShop = new SportShop(connectionString);
-            sportShop.Create("Stanga","Equipment",10,3333,"China",5555);
+            //sportShop.Create("Stanga","Equipment",10,3333,"China",5555);
+            var products = sportShop.GetALL();
+            foreach (var product in products)
+            {
+                Console.WriteLine(product);
+            }
         }
     }
 }
