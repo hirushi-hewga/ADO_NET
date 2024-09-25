b70da67a43c96005356a6b5d511ab6ede09af538
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-09-25 08:18:25 -0700

    Lesson (CRUDInterface)

---
+++ 2024-09-11 - Lesson (CRUD Interface)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.SqlClient;
+using System.Text;
+
+namespace _2024_09_11___Lesson__CRUD_Interface_
+{
+    class SportShop
+    {
+        //CRUD Interface
+        //[C]reate
+        //[R]ead
+        //[U]pdate
+        //[D]elete
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
